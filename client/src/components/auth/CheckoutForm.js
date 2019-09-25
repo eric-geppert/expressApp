4db11cabee8a2532dc83fa2e6042da7afc70c707
Eric <ericgeppert@att.net>
2019-09-25 15:42:29 -0500

    added error message, put in try catch so invalid credit card wont crash site, cleaned up comments in CheckoutFrom

--- client/src/components/auth/CheckoutForm.js
+++ client/src/components/auth/CheckoutForm.js
@@ -12,26 +12,28 @@ class CheckoutForm extends Component {
     super(props);
     this.state = { complete: false };
     this.submit = this.submit.bind(this);
-    //todo: add failure alerts
   }
 
   async submit(ev) {
     ev.preventDefault();
 
-    let { token } = await this.props.stripe.createToken({ name: 'Name' });
-    let response = await fetch('api/auth/charge', {
-      method: 'POST',
-      headers: { 'Content-Type': 'text/plain' },
-      body: token.id
-    });
+    try {
+      let { token } = await this.props.stripe.createToken({ name: 'Name' });
+      let response = await fetch('api/auth/charge', {
+        method: 'POST',
+        headers: { 'Content-Type': 'text/plain' },
+        body: token.id
+      });
 
-    if (response.ok) {
-      console.log('Purchase Complete!');
-      this.setState({ complete: true });
-      this.props.setAlert('Purchase complete!', 'success');
-      // setAlert('Purchase complete!', 'success');
-      // setAlert('Purchase complete (danger)!', 'danger');
-      alert('payment success!');
+      if (response.ok) {
+        console.log('Purchase Complete!');
+        this.setState({ complete: true });
+        this.props.setAlert('Purchase complete!', 'success');
+        alert('payment success!');
+      }
+    } catch (err) {
+      console.log('error caught by parent error : ' + err);
+      this.props.setAlert('There was a problem with your payment', 'danger');
     }
   }
 
@@ -52,73 +54,7 @@ class CheckoutForm extends Component {
     );
   }
 }
-// export default injectStripe(CheckoutForm);
-//need injectStripe
-
-// CheckoutForm.propTypes = {
-//   setAlert: PropTypes.func.isRequired,
-//   isAuthenticated: PropTypes.bool,
-//   paid: PropTypes.bool
-// };
-
-// const mapStateToProps = state => ({
-//   isAuthenticated: state.auth.isAuthenticated,
-//   //adding
-//   paid: state.auth.paid
-// });
-
-// export default connect(
-//   mapStateToProps,
-//   { setAlert }
-// )(injectStripe(CheckoutForm));
-
 export default connect(
   null,
   { setAlert }
 )(injectStripe(CheckoutForm));
-
-//--------
-// export default connect(
-//   mapStateToProps,
-//   { setAlert }
-// )(CheckoutForm);
-// )(injectStripe(CheckoutForm)); //works for stripe but not alerts
-
-//connect state to props paid instead of complete
-//todo include response function when connecting to props???
-
-//------
-// console.log('calling paystripe');
-// payStripe(this.props);
-//problems:
-//1.didnt' know what to pass,
-//2. wouldn't print conosle.log in action function payStripe
-
-//todo: remove dispatches
-
-//----
-
-// payStripe();
-//   async dispatch => ({
-//     type: BUY_SUCCESS,
-//     payload: response.data
-//   });
-// } else {
-//   dispatch({
-//     type: BUY_FAIL,
-//     payload: response.data
-//   });
-// }
-// else if(response.status===500){
-//   dispatch({
-//     type: BUY_FAIL,
-//     payload: res.data
-//     //todo: add payload of status??
-//   });
-// }
-// else if(response.status===400){
-//   dispatch({
-//     type: BUY_FAIL,
-//     payload: res.data
-//   });
-// }
