de7e376e5f4de3f7a02bdb456e044d2b2c664597
Eric <ericgeppert@att.net>
2019-10-01 12:11:27 -0500

    minor fix to text on register page

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
-import DashboardActions from './DashboardActions';
-import Experience from './Experience';
-import Education from './Education';
+// import DashboardActions from './DashboardActions';
+// import Experience from './Experience';
+// import Education from './Education';
 import { getCurrentProfile, deleteAccount } from '../../actions/profile';
 
 const Dashboard = ({
