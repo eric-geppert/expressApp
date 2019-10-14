ee5abc0a46e596e2431859acb0c68cf6e34b45a3
Eric <ericgeppert@att.net>
2019-10-14 11:11:09 -0500

    fixed formatting and added some profile into

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -25,16 +25,41 @@ const Dashboard = ({
     <Spinner />
   ) : (
     <Fragment>
-      <p> profile.id: {profile.customer.data[0].id} </p>
-      <p> profile.delinquent: {String(profile.customer.data[0].livemode)} </p>
-
       <h1 className='large text-primary'>Dashboard</h1>
       <p className='lead'>
-        <i className='fas fa-user' /> Welcome {user && user.name}
+        <i className='fas fa-user' /> Welcome to your profile page
+        {user && ' ' + user.name}
       </p>
+
+      <p> email: {profile.customer.data[0].email} </p>
+      <p>
+        subscription plan nickname:
+        {' ' + profile.customer.data[0].subscriptions.data[0].plan.nickname}
+      </p>
+      <p>
+        subscription cost: $
+        {profile.customer.data[0].subscriptions.data[0].plan.amount / 100}
+      </p>
+      <p>
+        charge every:
+        {' ' + profile.customer.data[0].subscriptions.data[0].plan.interval}
+      </p>
+      <p>
+        subscription status:
+        {' ' + profile.customer.data[0].subscriptions.data[0].status}
+      </p>
+      <p>
+        cancel at period end?:
+        {String(
+          ' ' +
+            profile.customer.data[0].subscriptions.data[0].cancel_at_period_end
+        )}
+      </p>
+      <p>
+        Payment is delinquent: {String(profile.customer.data[0].delinquent)}
+      </p>
+      {/* doesn't actually show days until payment due why? */}
       <Fragment>
-        <p>{profile.customer && profile.delinquent}</p>
-        {/* {profile && profile.customer.has_more} */}
         <p>
           You have successfully logged in would you like to purchase the full
           version of all our workout plans?
