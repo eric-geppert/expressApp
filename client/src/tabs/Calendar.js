118053e6faaee0b67e4f717aee97cbd8f084e8ac
Eric <ericgeppert@att.net>
2020-02-24 17:45:37 -0600

    basic calendar up with select a specific event functionality done

---
+++ client/src/tabs/Calendar.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+
+// import './App.css';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment);
+
+class MyCalendar extends Component {
+  state = {
+    selected: null,
+    events: [
+      {
+        start: new Date(),
+        end: new Date(moment().add(1, 'days')),
+        title: 'Some title',
+        workout: 'coolworkout'
+      },
+      {
+        start: new Date(moment().add(2, 'days')),
+        end: new Date(moment().add(2, 'days')),
+        title: 'workout2',
+        workout: '1000 pushups, 2000 sit ups, 10 miles'
+      }
+    ]
+  };
+
+  render() {
+    return this.state.selected === null || this.state.selected == undefined ? (
+      <div className='App'>
+        <Calendar
+          localizer={localizer}
+          defaultDate={new Date()}
+          defaultView='month'
+          events={this.state.events}
+          style={{ height: '100vh' }}
+          onDoubleClickEvent={e => this.setState({ selected: e.workout })}
+        />
+      </div>
+    ) : (
+      <p>{this.state.selected}</p>
+    );
+  }
+}
+
+export default MyCalendar;
