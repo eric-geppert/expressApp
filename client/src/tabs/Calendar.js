c77f92e5cb4ca69c9dadf3dd722eb87a8d6d55c7
Eric <ericgeppert@att.net>
2020-02-25 22:00:03 -0600

    deleted comments

--- client/src/tabs/Calendar.js
+++ client/src/tabs/Calendar.js
@@ -18,7 +18,6 @@ class MyCalendar extends Component {
   renderWorkouts() {
     var workoutArr = [];
 
-    console.log('this.state:', this.state);
     var i = 0;
     while (this.state.selected.workout[i].content != null) {
       workoutArr.push(<p key={i}>{this.state.selected.workout[i].content}</p>);
@@ -30,7 +29,6 @@ class MyCalendar extends Component {
   componentWillMount() {
     var eventArr = [];
     newWorkouts.forEach(function(element, index) {
-      // if (index - currentlyOnDay < 0)
       eventArr.push({
         start:
           index - currentlyOnDay < 0
@@ -45,14 +43,6 @@ class MyCalendar extends Component {
         workout: newWorkouts[index].contentwrapper,
         eventIndex: index
       });
-      // else
-      //   eventArr.push({
-      //     start: new Date(moment().add(index - currentlyOnDay, 'days')),
-      //     end: new Date(moment().add(index - currentlyOnDay, 'days')),
-      //     allDay: true,
-      //     title: newWorkouts[index].title,
-      //     workout: newWorkouts[index].contentwrapper
-      //   });
     });
     this.setState({ events: eventArr });
   }
@@ -60,32 +50,21 @@ class MyCalendar extends Component {
   render() {
     return this.state.selected === null || this.state.selected == undefined ? (
       <div className='App'>
-        {console.log('state: ', this.state)}
-        {console.log(
-          'newWorkouts[currentlyOnDay].contentwrapper: ',
-          newWorkouts[currentlyOnDay]
-        )}
-
         <Calendar
           localizer={localizer}
           defaultDate={new Date()}
           defaultView='month'
           events={this.state.events}
-          // events={this.state.events}
           style={{ height: '100vh' }}
           onDoubleClickEvent={e => {
-            //   console.log('e: ', e);
-            // }
             this.setState({
               selected: e
             });
-            // this.setState({ selected: e.workout })
           }}
         />
       </div>
     ) : (
       <div className='WorkoutParent'>
-        {/* {console.log('ee: ', this.state.selected.title)} */}
         <h2> {this.state.selected.title}</h2>
         {this.renderWorkouts()}
       </div>
