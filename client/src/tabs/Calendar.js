f9abef10277c751a639975625bc870cf9dfc41bb
Eric <ericgeppert@att.net>
2020-02-25 21:51:43 -0600

    calendar successfully dynamically renders all events based on what day of the program youre currently on, when you double click on a date it also dynamically rendors that specific title workout for that day adjusted for the lines that aren't null in the json file

--- client/src/tabs/Calendar.js
+++ client/src/tabs/Calendar.js
@@ -7,49 +7,88 @@ import newWorkouts from '../resources/newWorkouts.json';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 const localizer = momentLocalizer(moment);
+const currentlyOnDay = 2; //starts from day 0
 
 class MyCalendar extends Component {
   state = {
     selected: null,
-    events: [
-      {
-        start: new Date(),
-        end: new Date(moment().add(1, 'days')),
-        title: 'Some title',
-        workout: 'coolworkout'
-      },
-      {
-        start: new Date(moment().add(2, 'days')),
-        end: new Date(moment().add(2, 'days')),
-        title: 'workout2',
-        workout: '1000 pushups, 2000 sit ups, 10 miles'
-      }
-    ]
+    events: []
   };
 
-  render() {
-    {
-      console.log('newWorkouts', newWorkouts);
+  renderWorkouts() {
+    var workoutArr = [];
+
+    console.log('this.state:', this.state);
+    var i = 0;
+    while (this.state.selected.workout[i].content != null) {
+      workoutArr.push(<p key={i}>{this.state.selected.workout[i].content}</p>);
+      i++;
     }
+    return workoutArr;
+  }
+
+  componentWillMount() {
+    var eventArr = [];
+    newWorkouts.forEach(function(element, index) {
+      // if (index - currentlyOnDay < 0)
+      eventArr.push({
+        start:
+          index - currentlyOnDay < 0
+            ? new Date(moment().subtract(currentlyOnDay - index, 'days'))
+            : new Date(moment().add(index - currentlyOnDay, 'days')),
+        end:
+          index - currentlyOnDay < 0
+            ? new Date(moment().subtract(currentlyOnDay - index, 'days'))
+            : new Date(moment().add(index - currentlyOnDay, 'days')),
+        allDay: true,
+        title: newWorkouts[index].title,
+        workout: newWorkouts[index].contentwrapper,
+        eventIndex: index
+      });
+      // else
+      //   eventArr.push({
+      //     start: new Date(moment().add(index - currentlyOnDay, 'days')),
+      //     end: new Date(moment().add(index - currentlyOnDay, 'days')),
+      //     allDay: true,
+      //     title: newWorkouts[index].title,
+      //     workout: newWorkouts[index].contentwrapper
+      //   });
+    });
+    this.setState({ events: eventArr });
+  }
+
+  render() {
     return this.state.selected === null || this.state.selected == undefined ? (
       <div className='App'>
+        {console.log('state: ', this.state)}
+        {console.log(
+          'newWorkouts[currentlyOnDay].contentwrapper: ',
+          newWorkouts[currentlyOnDay]
+        )}
+
         <Calendar
           localizer={localizer}
           defaultDate={new Date()}
           defaultView='month'
           events={this.state.events}
+          // events={this.state.events}
           style={{ height: '100vh' }}
-          onDoubleClickEvent={
-            e =>
-              this.setState({
-                selected: newWorkouts[0].contentwrapper.content1
-              })
+          onDoubleClickEvent={e => {
+            //   console.log('e: ', e);
+            // }
+            this.setState({
+              selected: e
+            });
             // this.setState({ selected: e.workout })
-          }
+          }}
         />
       </div>
     ) : (
-      <p>{this.state.selected}</p>
+      <div className='WorkoutParent'>
+        {/* {console.log('ee: ', this.state.selected.title)} */}
+        <h2> {this.state.selected.title}</h2>
+        {this.renderWorkouts()}
+      </div>
     );
   }
 }
