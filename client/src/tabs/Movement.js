c6d414b5855c281f3aebc0726326e3f89ac86d12
Eric <ericgeppert@att.net>
2021-03-08 22:33:16 -0600

    added all youtube videos, abstracted them out to a json file

--- client/src/tabs/Movement.js
+++ client/src/tabs/Movement.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
+import React, { Fragment, Component } from 'react';
 import './Movement.css';
+import totalListOfVideos from '../resources/Movement.json';
 // import { Player } from 'video-react';
 // import Container from 'react-bootstrap/'
 // import Row from 'react-bootstrap/lib/Row'
@@ -7,58 +8,79 @@ import './Movement.css';
 // import TabContainer from 'react-bootstrap/lib/TabContainer'
 // import { Table } from "material-ui";
 
-const pStyle = {
-  fontWeight: 'bold',
-  fontSize: '18px'
-};
+/** have to use embed instead of watch */
 
-const headerStyle = {
-  textAlign: 'center',
-  fontSize: '40px',
-  marginBottom: '50px'
-};
+export const Movement = () => {
+  const renderAllvideos = () => {
+    var videoArr = [];
 
-export default class Movement extends Component {
-  render() {
-    return (
-      <div>
-        <div>
-          <h1 style={headerStyle}>Movement Library</h1>
+    totalListOfVideos.forEach((video) =>
+      videoArr.push(
+        <div id={video.label}>
+          <iframe
+            src={video.value}
+            frameBorder='0'
+            allow='autoplay; encrypted-media'
+            allowFullScreen
+            title='video'
+          />
+          <p>{String(video.label)}</p>
         </div>
-        <div className='container-of-boxes'>
-          <div id='seated db bench'>
-            <iframe
-              src='https://www.youtube.com/embed/VoA-Hhd5e1U' //have to use embed instead of watch
-              frameBorder='0'
-              allow='autoplay; encrypted-media'
-              allowFullScreen
-              title='video'
-              // name="Seated Dumbell BenchPress"
-            />
-            <p style={pStyle}>Seated Dumbell BenchPress</p>
-          </div>
-          <div id='db bench'>
-            <iframe
-              src='https://www.youtube.com/embed/O-zZAKgj0JY'
-              frameBorder='0'
-              allow='autoplay; encrypted-media'
-              allowFullScreen
-              title='video'
-            />
-            <p style={pStyle}>Dumbell BenchPress</p>
-          </div>
-          <div id='thruster'>
-            <iframe
-              src='https://www.youtube.com/embed/yP7k8eGvymA'
-              frameBorder='0'
-              allow='autoplay; encrypted-media'
-              allowFullScreen
-              title='video'
-            />
-            <p style={pStyle}>Thruster</p>
-          </div>
-        </div>
-      </div>
+      )
     );
-  }
+
+    return videoArr;
+  };
+
+  return (
+    <Fragment>
+      <h1>Movement Library</h1>
+      <div className='container-of-boxes'>{renderAllvideos()}</div>
+    </Fragment>
+  );
+};
+export default Movement;
+
+{
+  /* <div id='seated db bench'>
+          <iframe
+            src='https://www.youtube.com/embed/VoA-Hhd5e1U' //have to use embed instead of watch
+            frameBorder='0'
+            allow='autoplay; encrypted-media'
+            allowFullScreen
+            title='video'
+            // name="Seated Dumbell BenchPress"
+          />
+          <p>Seated Dumbell BenchPress</p>
+        </div>
+        <div id='db bench'>
+          <iframe
+            src='https://www.youtube.com/embed/O-zZAKgj0JY'
+            frameBorder='0'
+            allow='autoplay; encrypted-media'
+            allowFullScreen
+            title='video'
+          />
+          <p>Dumbell BenchPress</p>
+        </div>
+        <div id='thruster'>
+          <iframe
+            src='https://www.youtube.com/embed/yP7k8eGvymA'
+            frameBorder='0'
+            allow='autoplay; encrypted-media'
+            allowFullScreen
+            title='video'
+          />
+          <p>Thruster</p>
+        </div>
+        <div>
+          <iframe
+            src='https://www.youtube.com/embed/uRT0BAcMOgg'
+            frameBorder='0'
+            allow='autoplay; encrypted-media'
+            allowFullScreen
+            title='video'
+          />
+          <p>Air Squat</p>
+        </div> */
 }
