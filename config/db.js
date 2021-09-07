e0c783d793627fe5c50b3600045fd5fb437afd45
Eric <ericgeppert@att.net>
2021-09-06 23:20:46 -0500

    abstracted out db connection string

--- config/db.js
+++ config/db.js
@@ -4,12 +4,33 @@ const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
-      useNewUrlParser: true,
-      useCreateIndex: true
-    });
+    //todo: delete log
+    console.log('mongo connection string:', process.env.mongoTestURI);
+    if (process.env.mongoTestURI) {
+      /** we have to use "" around variable because of odd character but we need to trim them in windows
+       * do we need this for heroku?
+       */
+      console.log('mongoTestURI existsssssssssssssssss');
+      const testdb = process.env.mongoTestURI;
+      const noQuotes = testdb.substr(1, testdb.length - 2);
+      console.log('mongo testdb string:', testdb.substr(1, testdb.length - 2));
 
-    console.log('MongoDB Connected..');
+      await mongoose.connect(noQuotes, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+      });
+
+      console.log('MongoDB Connected..');
+      // set mongoTestURI=mongodb+srv://ericgeppert2:Henry4likes2run@devconnector-94qza.mongodb.net/test?retryWrites=true&w=majority
+    } else {
+      console.log('mongoTestURI DNEEEEEEEEEEEEEEE');
+    }
+    // await mongoose.connect(db, {
+    //   useNewUrlParser: true,
+    //   useCreateIndex: true,
+    // });
+
+    // console.log('MongoDB Connected..');
   } catch (err) {
     console.error(err.message);
     //exit process with failure
